ca2acc5d4e935b8801d6c177f3abffeb9e94d3c3
Muhammad Harib <haribharry7@gmail.com>
2026-05-18 22:39:48 +0500

    updated  the productviewer file

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <main>
         <Navbar />
         <Hero />
+        <ProductViewer />
     </main>
   )
 }
