4fc225ff77f3e666b064b7cad26717eb2c5704a9
Muhammad Harib <haribharry7@gmail.com>
2026-05-19 21:28:59 +0500

    updated  the productviewer file

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -7,14 +7,16 @@ const ProductViewer = () => {
         <section id='product-viewer'>
             <h2>Take a  closer look</h2>
             <div className="controls">
-                <p className='info'> MacbookPro 16'in  space Black</p>
+                <p className='info'> MacbookPro 16'in  {color}</p>
                 <div className="flex-center gap-5 mt-5">
                     <div className='color-control'>
                         <div
                          onClick={() => setColor('#adb5bd')}
                          className={clsx('bg-neutral-300', color === '#adb5bd' && 'active')}>
                         </div>
-                        <div className="bg-neutral-500">
+                        <div
+                         onClick={() => setColor('#2e2c2e')}
+                         className={clsx('bg-neutral-900', color === '#2e2c2e' && 'active')}>
                         </div>
                     </div>
 <div className="size-control">
