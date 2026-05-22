1a74b37a1e9523bca0ed340d962ca6025315b85a
Muhammad Harib <haribharry7@gmail.com>
2026-05-22 23:47:41 +0500

    added remaining components and half of performance component

--- src/app.jsx
+++ src/app.jsx
@@ -3,8 +3,13 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import Showcase from './components/Showcase'
+import Performance from './components/Performance'
+import Features from './components/Features'
+import Highlights from './components/Highlights'
+import Footer from './components/Footer'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
+import { Features } from 'tailwindcss'
 gsap.registerPlugin(ScrollTrigger)
 
 
@@ -15,6 +20,10 @@ const App = () => {
         <Hero />
         <ProductViewer />
         <Showcase />
+        <Performance />
+        <Features />
+        <Highlights />
+        <Footer />
     </main>
   )
 }
